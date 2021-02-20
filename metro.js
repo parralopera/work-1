c4e81c51d9c79347dda933846e581855b11b3254
parralopera <54422368+parralopera@users.noreply.github.com>
2021-02-20 11:43:06 -0500

    Add files via upload

---
+++ metro.js
@@ -0,0 +1,11 @@
+var pasenger = pickRamdom({
+	'mas de 50',
+	'mas de 100',
+	'menos de 20',
+	'nadie tomo el metro',
+});
+
+print(pasenger);
+if (pasenger !== 'nadie tomo el metro') {
+	print('Fuera de Servicio')
+}
